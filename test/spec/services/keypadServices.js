1f1c055cb91b86307042996cf6a8ea1acaef3f72
Vtanathip <tanathip.se@gmail.com>
2013-07-13 10:26:30 +0700

    add unit test for keypad services {add,remove,services}

--- test/spec/services/keypadServices.js
+++ test/spec/services/keypadServices.js
@@ -3,9 +3,15 @@ describe('Keypad Services : ', function(){
     // load the controller's module
     beforeEach(module('angularSPMApp'));
 
-    var serviceKeypad;
+    var serviceKeypad,
+        scope,
+        TopUpTrueMoveCtrl;
     // Initialize the controller and a mock scope
-    beforeEach(inject(function ($injector) {
+    beforeEach(inject(function ($controller,$rootScope,$injector) {
+        scope = $rootScope.$new();
+        TopUpTrueMoveCtrl = $controller('TopUpTrueMoveCtrl', {
+            $scope: scope
+        });
         serviceKeypad = $injector.get('keypad');
     }));
 
@@ -13,5 +19,32 @@ describe('Keypad Services : ', function(){
         expect(serviceKeypad).not.toBeNull();
     });
 
+    it('keypad should add number when called',function(){
+        scope.mobileno = [];
+        //add mobile number to array
+        expect(serviceKeypad.add(scope,1)).not.toBeNull();
+        //mobile array should equal 1
+        expect(scope.mobileno.length).toBe(1);
+    });
+
+    it('keypad should remove number in array when call remove',function(){
+        scope.mobileno = [];
+        //add mobile number to array
+        expect(serviceKeypad.add(scope,1)).not.toBeNull();
+        //mobile array now should be 1
+        expect(scope.mobileno.length).toBe(1);
+        //remove mobile in array
+        expect(serviceKeypad.remove(scope)).not.toBeNull();
+        //mobile array now should be 1
+        expect(scope.mobileno.length).toBe(0);
+    });
+
+    it('keypad should format number',function(){
+        scope.mobileno = ['0','8','6','8','1','8','5','0','5','5'];
+        //add mobile number to array
+        expect(serviceKeypad.formatMobile(scope)).not.toBeNull();
+        //mobile array now should be 12
+        expect(scope.mobileno.length).toBe(12);
+    });
 
 });
